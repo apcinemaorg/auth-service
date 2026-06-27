4081284edc3f5136e3c65929b75d92cfe2691304
apervashov <videoman496@gmail.com>
2026-06-27 15:24:33 +0200

    feat: implement OTP send and verify via Redis

--- src/modules/otp/otp.module.ts
+++ src/modules/otp/otp.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { OtpService } from './otp.service';
-import { OtpController } from './otp.controller';
 
 @Module({
-  controllers: [OtpController],
   providers: [OtpService],
+  exports: [OtpService],
 })
 export class OtpModule {}
--- src/modules/otp/otp.service.ts
+++ src/modules/otp/otp.service.ts
@@ -1,14 +1,36 @@
 import { RedisService } from '@/infrastructure/redis/redis.service';
+import { SendOtpRequest, VerifyOtpRequest } from '@apcinema/contracts/gen/auth';
 import { Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { createHash } from 'crypto';
 
 @Injectable()
 export class OtpService {
 
     public constructor(private readonly redisService: RedisService) {}
-    private generateCode(): string {
-        const code = Math.floor(100000 + Math.random() * 900000);
+    public async sendOtp(data: SendOtpRequest): Promise<number> {
+        const { identifier, type } = data;
+        const code = this.generateCode();
         const hash = createHash('sha256').update(code.toString()).digest('hex');
-        return hash;
+
+        await this.redisService.set(`otp:${type}:${identifier}`, hash, 'EX', 300);
+        return code;
+    }
+    private generateCode(): number {
+        return Math.floor(100000 + Math.random() * 900000);
+    }
+
+    public async verifyOtp(data: VerifyOtpRequest): Promise<boolean> {
+        const { identifier, type, code } = data;
+        const storedHash = await this.redisService.get(`otp:${type}:${identifier}`);
+        const newHash = createHash('sha256').update(code.toString()).digest('hex');
+        if(!storedHash){
+            throw new RpcException('Invalid OTP');
+        }
+        if(storedHash !== newHash){
+            throw new RpcException('Invalid OTP');
+        }
+        await this.redisService.del(`otp:${type}:${identifier}`);
+        return true;
     }
 }

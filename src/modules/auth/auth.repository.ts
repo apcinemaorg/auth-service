a1ae192a5b286a4b79ddc6b5841a01300b7b6240
apervashov <videoman496@gmail.com>
2026-06-27 12:55:37 +0200

    feat: implement SendOtp with account lookup and creation

---
+++ src/modules/auth/auth.repository.ts
@@ -0,0 +1,31 @@
+import { PrismaService } from "@/infrastructure/prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
+import { Account } from "@prisma/generated/client";
+import { AccountCreateInput } from "@prisma/generated/models";
+
+@Injectable()
+export class AuthRepository {
+    public constructor(private readonly prismaService: PrismaService) {}
+
+    public async findAccountByPhone(phone: string): Promise<Account | null>{
+        return await this.prismaService.account.findUnique({
+            where: {
+                phone: phone
+            }
+        })
+    }
+
+    public async findAccountByEmail(email: string): Promise<Account | null>{
+        return await this.prismaService.account.findUnique({
+            where: {
+                email: email
+            }
+        })
+    }
+
+    public async createAccount(data: AccountCreateInput): Promise<Account>{
+        return await this.prismaService.account.create({
+            data: data
+        })
+    }
+}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import type{ SendOtpRequest } from '@apcinema/contracts/gen/auth';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { AuthRepository } from './auth.repository';
+import { Account } from '@prisma/generated/client';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    public constructor(private readonly authRepository: AuthRepository) {}
+    public async sendOtp(data: SendOtpRequest){
+        const {identifier, type} = data;
+        let account: Account | null = null
+        if(type === 'phone'){
+            account = await this.authRepository.findAccountByPhone(identifier)
+        }
+        if(type === 'email'){
+            account = await this.authRepository.findAccountByEmail(identifier)
+        }
+        if(!account){
+            account = await this.authRepository.createAccount({
+                phone: type === 'phone' ? identifier : undefined,
+                email: type === 'email' ? identifier : undefined,
+            })
+        }
+        return {ok:true};
+    }
+}
